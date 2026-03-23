27b72111b91bfaaf888f62a7356392ddbf0c68eb
vishwajit kumar <vishawjitkumar@gmail.com>
2026-03-23 16:54:07 +0530

    added file for db

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080", // later change to ALB URL
+  baseURL: import.meta.env.VITE_API_URL,// later change to ALB URL
 });
 
 export const getData = () => API.get("/data");
