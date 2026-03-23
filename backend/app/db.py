e7cfcd31b86307b24e3e364a9bae1253b031a41d
vishwajit kumar <vishawjitkumar@gmail.com>
2026-03-23 16:12:35 +0530

    added file for db

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,7 +1,21 @@
+import time
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import Config
 
-engine = create_engine(Config.DB_URL)
-SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def get_engine():
+    while True:
+        try:
+            engine = create_engine(Config.DB_URL)
+            conn = engine.connect()
+            conn.close()
+            print("✅ Connected to DB")
+            return engine
+        except Exception as e:
+            print("⏳ Waiting for DB...", e)
+            time.sleep(2)
+
+engine = get_engine()
+SessionLocal = sessionmaker(bind=engine)
