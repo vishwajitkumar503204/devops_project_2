616ff16368ef487453e5d6bdd38ed4b35ac0e08b
vishwajit kumar <vishawjitkumar@gmail.com>
2026-03-23 17:38:18 +0530

    fix for db

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -1,27 +1,21 @@
 import time
-import requests
 from app.db import SessionLocal
-from app.models import APIData
-from app.config import Config
+from app.models import Data
 
-def start_worker():
-    print("Worker started...")
+while True:
+    try:
+        db = SessionLocal()
 
-    while True:
-        try:
-            response = requests.get(Config.API_URL, timeout=5)
-            data = response.json()
+        new_data = Data(value="Sample data")
+        db.add(new_data)
+        db.commit()
 
-            db = SessionLocal()
+        print("Data saved")
 
-            record = APIData(data=data)
-            db.add(record)
-            db.commit()
-            db.close()
+    except Exception as e:
+        print("Error:", e)
 
-            print("Data saved")
+    finally:
+        db.close()
 
-        except Exception as e:
-            print("Error:", e)
-
-        time.sleep(Config.POLL_INTERVAL)
+    time.sleep(5)
